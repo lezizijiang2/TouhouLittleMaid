db9f71f67ef9ea8d4b701cfe25483ae41cda29cb
Azumic <95330698+Azumic@users.noreply.github.com>
2025-01-11 02:13:09 +0800

    同步Merge pull request #641 #e278fe6a

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/tacz/task/TaskGunAttack.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/compat/tacz/task/TaskGunAttack.java
@@ -6,6 +6,7 @@ import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunAttackStrafing
 import com.github.tartaricacid.touhoulittlemaid.compat.tacz.ai.GunShootTargetTask;
 import com.github.tartaricacid.touhoulittlemaid.config.subconfig.MaidConfig;
 import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.task.MaidRangedWalkToTarget;
+import com.github.tartaricacid.touhoulittlemaid.entity.ai.brain.task.MaidUseShieldTask;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.init.InitItems;
 import com.github.tartaricacid.touhoulittlemaid.init.InitSounds;
@@ -74,13 +75,15 @@ public class TaskGunAttack implements IRangedAttackTask {
         Behavior<EntityMaid> gunWalkTargetTask = new GunWalkToTarget(0.6f);
         Behavior<EntityMaid> gunAttackStrafingTask = new GunAttackStrafingTask();
         Behavior<EntityMaid> gunShootTargetTask = new GunShootTargetTask();
+        MaidUseShieldTask maidUseShieldTask = new MaidUseShieldTask();
 
         return Lists.newArrayList(
                 Pair.of(5, supplementedTask),
                 Pair.of(5, findTargetTask),
                 Pair.of(5, gunWalkTargetTask),
                 Pair.of(5, gunAttackStrafingTask),
-                Pair.of(5, gunShootTargetTask)
+                Pair.of(5, gunShootTargetTask),
+                Pair.of(5, maidUseShieldTask)
         );
     }
 
