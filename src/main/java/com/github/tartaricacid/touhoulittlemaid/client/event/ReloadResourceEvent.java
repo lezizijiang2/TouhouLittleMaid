24e774bed01758bbea9f7a098c9966fce6a4b7c7
tartaric_acid <baka943@qq.com>
2025-01-17 19:59:08 +0800

    同步部分修正 #60751c11

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/ReloadResourceEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/event/ReloadResourceEvent.java
@@ -5,9 +5,11 @@ import com.github.tartaricacid.touhoulittlemaid.client.animation.inner.InnerAnim
 import com.github.tartaricacid.touhoulittlemaid.client.resource.CustomPackLoader;
 import com.github.tartaricacid.touhoulittlemaid.client.resource.GeckoModelLoader;
 import com.github.tartaricacid.touhoulittlemaid.client.resource.models.PlayerMaidModels;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.commons.lang3.time.StopWatch;
@@ -16,28 +18,10 @@ import java.util.concurrent.TimeUnit;
 
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-public final class ReloadResourceEvent {
-    public static final ResourceLocation TANK_INPUT_SLOT = new ResourceLocation(TouhouLittleMaid.MOD_ID, "slot/tank_input_slot");
-    public static final ResourceLocation TANK_OUTPUT_SLOT = new ResourceLocation(TouhouLittleMaid.MOD_ID, "slot/tank_output_slot");
-    public static final ResourceLocation EMPTY_MAINHAND_SLOT = new ResourceLocation(TouhouLittleMaid.MOD_ID, "slot/empty_mainhand_slot");
-    public static final ResourceLocation BLOCK_ATLAS_TEXTURE = new ResourceLocation("textures/atlas/blocks.png");
-    public static final ResourceLocation EMPTY_BACK_SHOW_SLOT = new ResourceLocation(TouhouLittleMaid.MOD_ID, "slot/empty_back_show_slot");
-
+public final class ReloadResourceEvent extends SimplePreparableReloadListener<Void> {
     @SubscribeEvent
-    public static void onTextureStitchEventPost(TextureStitchEvent.Post event) {
-        if (BLOCK_ATLAS_TEXTURE.equals(event.getAtlas().location())) {
-            reloadAllPack();
-        }
-    }
-
-    @SubscribeEvent
-    public static void onTextureStitchEventPre(TextureStitchEvent.Pre event) {
-        if (BLOCK_ATLAS_TEXTURE.equals(event.getAtlas().location())) {
-            event.addSprite(TANK_INPUT_SLOT);
-            event.addSprite(TANK_OUTPUT_SLOT);
-            event.addSprite(EMPTY_MAINHAND_SLOT);
-            event.addSprite(EMPTY_BACK_SHOW_SLOT);
-        }
+    public static void onRegister(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(new ReloadResourceEvent());
     }
 
     public static void reloadAllPack() {
@@ -50,5 +34,15 @@ public final class ReloadResourceEvent {
         }
         watch.stop();
         TouhouLittleMaid.LOGGER.info("Model loading time: {} ms", watch.getTime(TimeUnit.MICROSECONDS) / 1000.0);
+    }
+
+    @Override
+    protected Void prepare(ResourceManager pResourceManager, ProfilerFiller pProfiler) {
+        reloadAllPack();
+        return null;
+    }
+
+    @Override
+    protected void apply(Void pObject, ResourceManager pResourceManager, ProfilerFiller pProfiler) {
     }
 }
