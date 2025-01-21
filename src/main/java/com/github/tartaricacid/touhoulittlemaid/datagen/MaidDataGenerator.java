b53f1055203924b4dd4139c93a70f9b9d785e295
tartaric_acid <baka943@qq.com>
2025-01-21 23:05:55 +0800

    同步部分内容更新 #c0f51309

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/MaidDataGenerator.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/MaidDataGenerator.java
@@ -2,7 +2,10 @@ package com.github.tartaricacid.touhoulittlemaid.datagen;
 
 import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.datagen.tag.EntityTypeGenerator;
+import com.github.tartaricacid.touhoulittlemaid.datagen.tag.TagBlock;
+import com.github.tartaricacid.touhoulittlemaid.datagen.tag.TagItem;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,6 +18,8 @@ public class MaidDataGenerator {
         DataGenerator generator = event.getGenerator();
         ExistingFileHelper helper = event.getExistingFileHelper();
 
+        BlockTagsProvider blocktagsprovider = new TagBlock(generator);
+
         // Advancement
         generator.addProvider(true, new AdvancementGenerator(generator, helper));
 
@@ -23,5 +28,6 @@ public class MaidDataGenerator {
 
         // Tags
         generator.addProvider(event.includeServer(), new EntityTypeGenerator(generator, event.getExistingFileHelper()));
+        generator.addProvider(event.includeServer(), new TagItem(generator, blocktagsprovider, TouhouLittleMaid.MOD_ID, helper));
     }
 }
---
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/tag/TagBlock.java
@@ -0,0 +1,17 @@
+package com.github.tartaricacid.touhoulittlemaid.datagen.tag;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+
+public class TagBlock extends BlockTagsProvider {
+    public TagBlock(DataGenerator dataGenerator) {
+        super(dataGenerator);
+    }
+
+    public TagBlock(DataGenerator dataGenerator, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+        super(dataGenerator, modId, existingFileHelper);
+    }
+}
