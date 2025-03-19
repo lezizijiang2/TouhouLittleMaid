04fbd9d9a7323888880107c2c74daca7b7500b84
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2025-03-20 00:32:31 +0800

    Merge pull request #696
    * 死亡不掉落规则打开后会保留p点
    
    * 将配置文件考虑其中

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/event/EntityDeathEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/event/EntityDeathEvent.java
@@ -1,10 +1,19 @@
 package com.github.tartaricacid.touhoulittlemaid.event;
 
+import com.github.tartaricacid.touhoulittlemaid.config.subconfig.MiscConfig;
+import com.github.tartaricacid.touhoulittlemaid.data.MaidNumAttachment;
+import com.github.tartaricacid.touhoulittlemaid.data.PowerAttachment;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameRules;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+import static com.github.tartaricacid.touhoulittlemaid.init.InitDataAttachment.MAID_NUM;
+import static com.github.tartaricacid.touhoulittlemaid.init.InitDataAttachment.POWER_NUM;
 
 @EventBusSubscriber
 public class EntityDeathEvent {
@@ -14,5 +23,21 @@ public class EntityDeathEvent {
         if (causingEntity instanceof EntityMaid maid) {
             maid.getKillRecordManager().onTargetDeath(maid, event.getEntity());
         }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerCloned(PlayerEvent.Clone event) {
+        Player newEntity = event.getEntity();
+        Player oldEntity = event.getOriginal();
+        boolean wasDeath = event.isWasDeath();
+        boolean isKeep = newEntity.level.getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY);
+
+        PowerAttachment power = oldEntity.getData(POWER_NUM);
+        MaidNumAttachment maidNum = oldEntity.getData(MAID_NUM);
+        if (wasDeath && !isKeep) {
+            power.min(MiscConfig.PLAYER_DEATH_LOSS_POWER_POINT.get().floatValue());
+        }
+        newEntity.setData(POWER_NUM, power);
+        newEntity.setData(MAID_NUM, maidNum);
     }
 }
