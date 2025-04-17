af2c8304cda7c1869d27e79a1160502643467e52
tartaric_acid <baka943@qq.com>
2025-04-17 21:40:50 +0800

    完成 1.19.2 的同步 - 目前还有渲染 bug

---
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/ai/service/tts/gptsovits/request/TTSGptSovitsRequest.java
@@ -0,0 +1,67 @@
+package com.github.tartaricacid.touhoulittlemaid.ai.service.tts.gptsovits.request;
+
+import com.github.tartaricacid.touhoulittlemaid.ai.manager.setting.Site;
+import com.github.tartaricacid.touhoulittlemaid.ai.service.tts.TTSRequest;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
+public class TTSGptSovitsRequest implements TTSRequest {
+    @SerializedName("text")
+    private String text;
+
+    @SerializedName("text_lang")
+    private String textLang;
+
+    @SerializedName("ref_audio_path")
+    private String refAudioPath;
+
+    @SerializedName("prompt_lang")
+    private String promptLang;
+
+    @SerializedName("prompt_text")
+    private String promptText;
+
+    @SerializedName("text_split_method")
+    private String textSplitMethod;
+
+    @SerializedName("media_type")
+    private String mediaType = "ogg";
+
+    @SerializedName("streaming_mode")
+    private boolean streamingMode = true;
+
+    public static TTSGptSovitsRequest create() {
+        return new TTSGptSovitsRequest();
+    }
+
+    private TTSGptSovitsRequest() {
+    }
+
+    public TTSGptSovitsRequest setSiteExtraArgs(Site site) {
+        Map<String, String> extraArgs = site.getExtraArgs();
+        if (extraArgs.containsKey("ref_audio_path")) {
+            this.refAudioPath = extraArgs.get("ref_audio_path");
+        }
+        if (extraArgs.containsKey("prompt_lang")) {
+            this.promptLang = extraArgs.get("prompt_lang");
+        }
+        if (extraArgs.containsKey("prompt_text")) {
+            this.promptText = extraArgs.get("prompt_text");
+        }
+        if (extraArgs.containsKey("text_split_method")) {
+            this.textSplitMethod = extraArgs.get("text_split_method");
+        }
+        return this;
+    }
+
+    public TTSGptSovitsRequest setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public TTSGptSovitsRequest setTextLang(String textLang) {
+        this.textLang = textLang;
+        return this;
+    }
+}
