88bef073bd63b4643d294f9960128e6ba02b0c04
lezizijiang2 <viger123@outlook.com>
2025-04-30 11:41:17 +0800

    fix: 修正1.21.1的附魔支持。现在可以正确为御币附魔耐久和经验修补，为存在耐久的饰品附魔耐久。

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/DataGenerator.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/DataGenerator.java
@@ -51,6 +51,7 @@ public class DataGenerator {
                 .addProvider(packOutput -> new TagBlock(packOutput, registries, TouhouLittleMaid.MOD_ID, existingFileHelper));
         vanillaPack.addProvider(
                 packOutput -> new TagItem(packOutput, registries, blockTagsProvider.contentsGetter(), TouhouLittleMaid.MOD_ID, existingFileHelper));
+        vanillaPack.addProvider(packOutput -> new TagEnchantment(packOutput, registries, TouhouLittleMaid.MOD_ID, existingFileHelper));
         generator.addProvider(event.includeServer(), new DamageTypeGenerator(pack, event.getLookupProvider(), event.getExistingFileHelper()));
         generator.addProvider(event.includeServer(), new EntityTypeGenerator(pack, event.getLookupProvider(), event.getExistingFileHelper()));
         generator.addProvider(event.includeServer(), new TagRecipeSerializer(pack, event.getLookupProvider(), event.getExistingFileHelper()));
