ab8a2d84f72052b29df09877a1eb70ac19bc3150
lezizijiang2 <viger123@outlook.com>
2025-04-30 12:36:23 +0800

    revert: 允许饰品附魔经验修补

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/tag/TagEnchantment.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/datagen/tag/TagEnchantment.java
@@ -1,29 +1,19 @@
 package com.github.tartaricacid.touhoulittlemaid.datagen.tag;
 
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EnchantmentTagsProvider;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
-import static com.github.tartaricacid.touhoulittlemaid.util.ResourceLocationUtil.getResourceLocation;
-
 public class TagEnchantment extends EnchantmentTagsProvider {
-    public static final TagKey<Enchantment> BAUBLE_INCOMPATIBLE =
-            TagKey.create(Registries.ENCHANTMENT, getResourceLocation("bauble_incompatible"));
-
     public TagEnchantment(PackOutput output, CompletableFuture<HolderLookup.Provider> completableFuture, String modId, @Nullable ExistingFileHelper existingFileHelper) {
         super(output, completableFuture, modId, existingFileHelper);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
-        this.tag(BAUBLE_INCOMPATIBLE).add(Enchantments.MENDING);
     }
 }
--- src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemDamageableBauble.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemDamageableBauble.java
@@ -1,14 +1,7 @@
 package com.github.tartaricacid.touhoulittlemaid.item;
 
-import com.github.tartaricacid.touhoulittlemaid.datagen.tag.TagEnchantment;
-import net.minecraft.core.Holder;
-import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-
-import java.util.Map;
 
 public class ItemDamageableBauble extends Item {
     public ItemDamageableBauble(int durability) {
@@ -19,11 +12,4 @@ public class ItemDamageableBauble extends Item {
     public boolean isFoil(ItemStack stack) {
         return true;
     }
-
-    @Override
-    public boolean supportsEnchantment(ItemStack stack, Holder<Enchantment> enchantment) {
-        // 检查是否含有不兼容附魔, 注册表在 TagEnchantment 中。此处应当仅有mending
-        return super.supportsEnchantment(stack, enchantment) && !enchantment.is(TagEnchantment.BAUBLE_INCOMPATIBLE);
-    }
-
 }
